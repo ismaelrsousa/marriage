60fac4999d66a125a7d6d9841db44601ff7825f5
Ismael Sousa <42844893+ismaelrsousa@users.noreply.github.com>
2026-08-29 21:59:30 -0300

    Change base path configuration in vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/marriage/" : "/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     watch: {
